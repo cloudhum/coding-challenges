5acc6058ce1dfc4d76cfcc6eacb7f18254fe833d
cloudhum <turbulentbees@gmail.com>
2022-03-31 11:55:05 -0700

    Anagram detection

---
+++ anagram-detection.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/529eef7a9194e0cbc1000255/javascript
+
+var isAnagram = function (test, original) {
+    return test.toLowerCase().split("").sort().join("") == original.toLowerCase().split("").sort().join("");
+};
