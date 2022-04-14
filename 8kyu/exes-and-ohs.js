2a11fd681ccb8d74d913ca6c103e17ac955d2b04
cloudhum <turbulentbees@gmail.com>
2022-04-14 11:50:51 -0700

    Exes and Ohs

---
+++ 8kyu/exes-and-ohs.js
@@ -0,0 +1,18 @@
+function XO(str) {
+  
+    let xs = 0;
+    let os = 0;
+    
+    for (let i = 0; i < str.length; i++){
+      if (str[i] == "x"|| str[i] == "X"){
+        xs++;
+      }
+      
+      if(str[i] == "o" || str[i] == "O"){
+        os++;
+      }
+    }
+   
+    return (xs == 0 && os == 0)||(xs == os);
+    
+  }
