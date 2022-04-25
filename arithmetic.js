f489b30b59381ae1e7d1e675ed0042d8a8f83121
cloudhum <turbulentbees@gmail.com>
2022-04-25 13:20:53 -0700

    Function 2-squaring an argument

---
+++ arithmetic.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/583f158ea20cfcbeb400000a
+
+function arithmetic(a, b, operator) {
+    if (operator == "add") return a + b;
+    if (operator == "subtract") return a - b;
+    if (operator == "multiply") return a * b;
+    if (operator == "divide") return a / b;
+}
