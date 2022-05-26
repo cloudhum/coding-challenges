ef71f82cd7aa5679a9cd6616ed51d2a5582da277
cloudhum <turbulentbees@gmail.com>
2022-05-26 12:11:07 -0700

    Pre-FizzBuzz

---
+++ pre-fizz-buzz.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/569e09850a8e371ab200000b/javascript
+
+function preFizz(n) {
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+        result.push(i);
+    }
+    return result;
+}
