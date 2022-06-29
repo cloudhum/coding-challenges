88cd3e5530e9871c66f2011a743de02fdee980c0
cloudhum <turbulentbees@gmail.com>
2022-06-28 20:54:38 -0700

    repeatIt

--- repeatIt.js
+++ repeatIt.js
@@ -1,10 +1,14 @@
 // https://www.codewars.com/kata/557af9418895e44de7000053/train/javascript
 
-var repeatIt = function(str, n) {
-  if (typeof str !== "string") return "Not a string";
-  let repeatedStr = "";
-  for (let i = 0; i < n; i++) {
-    repeatedStr += str;
-  }
-  return repeatedStr;
+var repeatIt = function (str, n) {
+    if (typeof str !== "string") return "Not a string";
+    let repeatedStr = "";
+    for (let i = 0; i < n; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
+}
+
+var repeatIt = function (str, n) {
+    return typeof str == "string" ? str.repeat(n) : "Not a string"
 }
