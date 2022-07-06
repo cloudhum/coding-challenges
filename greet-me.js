31216bea3781c3c99a77932d02cc52bc12a99114
cloudhum <turbulentbees@gmail.com>
2022-07-05 20:32:07 -0700

    Greet Me

---
+++ greet-me.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/535474308bb336c9980006f2/train/javascript
+
+var greet = function (name) {
+    return `Hello ${name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase()}!`
+};
