a42f195e0ee69051cac097fc3646c7d489cdcb80
cloudhum <turbulentbees@gmail.com>
2022-07-12 11:56:47 -0700

    Linked List Cycle

---
+++ linked-list-cycle.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    let map = []
+    while (head) {
+        if (map.includes(head)) {
+            return true
+        } else {
+            map.push(head)
+            head = head.next
+        }
+    }
+    return false
+};
