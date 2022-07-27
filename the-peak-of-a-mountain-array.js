04f9305372ad514594eb9980747d2c2c25b5c710
cloudhum <turbulentbees@gmail.com>
2022-07-26 19:25:56 -0700

    Updated The Peak of a Mountain Array

--- the-peak-of-a-mountain-array.js
+++ the-peak-of-a-mountain-array.js
@@ -1,9 +1,7 @@
 /*
     A mountain array is defined as an array that
-
         has at least 3 elements
         has an element with the largest value called "peak", with index k. The array elements monotonically increase from the first element to A[k], and then monotonically decreases from A[k + 1] to the last element of the array. Thus creating a "mountain" of numbers.
-
     That is, given A[0]<...<A[k-1]<A[k]>A[k+1]>...>A[n-1], we need to find the index k. Note that the peak element is neither the first nor the last element of the array.
 */
 
