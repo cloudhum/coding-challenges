d9cee70811ebb0c27eaee000bccc6d020e6e6519
cloudhum <turbulentbees@gmail.com>
2022-08-05 10:44:35 -0700

    Updated: Remove Element

--- remove-element.js
+++ remove-element.js
@@ -2,7 +2,6 @@
 
 var removeElement = function (nums, val) {
     let slow = 0
-    let boundary = -1
     for (let fast = 0; fast < nums.length; fast++) {
         if (nums[fast] !== val) {
             [nums[fast], nums[slow]] = [nums[slow], nums[fast]]
